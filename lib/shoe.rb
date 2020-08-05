799865bbf3d2e597dbe55c4fe422621409a5766f
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-08-05 09:58:05 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,70 +1,10 @@
-# Make your shoe class here!
-
 class Shoe
-  
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+   
   def initialize(brand)
     @brand = brand
   end
-  
-  def brand
-    @brand
-  end 
-  
-  def color=(color)
-    @color = color 
-  end
-  
-  def color
-    @color
-  end 
-  
-  def size=(num)
-    @size = size
-  end 
-  
-  def size
-    @size
-  end 
-  
-  def material=(material)
-    @material = material
-  end 
-  
-  def material
-    @material 
-  end 
-  
-  def condition=(condition)
-    @condition
-  end 
-  
-  def condition
-    @condition
-  end
-  
-  def cobble=(cobble)
-    @cobble = cobble
-  end
-  
-  def cobble
-    @cobble
-  end
-end
-
- 
- # but better yet, to clean up the code
- 
- class Shoe
-   attr_accessor :color, :size, :material, :condition
-   attr_reader :brand
-   
-   def initialize(brand)
-    @brand = brand
-  end
-  
-  def brand
-    @brand
-  end 
   
   def cobble
     @condition = new 
