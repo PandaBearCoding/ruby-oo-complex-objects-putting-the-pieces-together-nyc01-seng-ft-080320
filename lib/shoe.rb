6d1afae79cc6a1987ea2dcac53d2a3b0d52a3a9d
Madeline Rose Stalter <madelinerose16@outlook.com>
2020-08-05 09:47:09 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,62 +2,55 @@
 
 class Shoe
   
-  def shoe
+  def initialize(brand)
+    @brand= brand
   end
   
-end
-
+  def brand
+    @brand
+  end 
   
-  
-  
-  
-  describe 'Shoe' do
-  let(:shoe) { Shoe.new("Nike") }
-
-  describe '::new' do
-    it 'gets initialized with a brand' do
-      expect{Shoe.new("Adidas")}.to_not raise_error
-    end
+  def color=(color)
+    @color = color 
   end
-
-  describe 'properties' do
-    it 'has a brand' do
-      # When must the brand be assigned for this to work?
-      expect(shoe.brand).to eq("Nike")
-    end
-
-    it 'has a color' do
-      shoe.color = "red"
-      expect(shoe.color).to eq("red")
-    end
-
-    it 'has a size' do
-      shoe.size = 9.5
-      expect(shoe.size).to eq(9.5)
-    end
-
-    it 'has a material' do
-      shoe.material = "suede"
-      expect(shoe.material).to eq("suede")
-    end
-
-    it 'has a condition' do
-      shoe.condition = "tattered"
-      expect(shoe.condition).to eq("tattered")
-    end
+  
+  def color
+    @color
+  end 
+  
+  def size=(num)
+    @size = size
+  end 
+  
+  def size
+    @size
+  end 
+  
+  def material=(material)
+    @material = material
+  end 
+  
+  def material
+    @material 
+  end 
+  
+  def conition=(condition)
+    @condition
+  end 
+  
+  def condition
+    @condition
   end
-
-  describe '#cobble' do
-    it 'says that the shoe has been repaired' do
-      expect($stdout).to receive(:puts).with("Your shoe is as good as new!")
-      shoe.cobble
-    end
-
-    it 'makes the shoe\'s condition new' do
-      shoe.condition = "old"
-      shoe.cobble
-      expect(shoe.condition).to eq("new")
-    end
+  
+  def cobble=(cobble)
+    @cobble = cobble
+  end
+  
+  def cobble
+    @cobble
   end
 end
+
+ 
+  
   
